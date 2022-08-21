354907eb5b9fadb0e228db020e1cd3f6c360a77e
ChadScarborough <chad.d.scarborough@gmail.com>
2022-08-21 15:19:57 -0700

    Create static Functions class

---
+++ ChessBoard/Globals/Functions.cs
@@ -0,0 +1,11 @@
+﻿namespace ChessBoard.Globals
+{
+    public static class Functions
+    {
+        public static void GuardAgainstOffBoardMoves(int rank, int file)
+        {
+            if (rank < 0 || rank >= 8 || file < 0 || file >= 8) 
+                throw new Exception("Piece cannot move off the board");
+        }
+    }
+}
