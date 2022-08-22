1e5b4c2b1b71cec3bd060723811df1bf29c4c445
ChadScarborough <chad.d.scarborough@gmail.com>
2022-08-22 16:23:37 -0700

    Add rook to piece factory

--- ChessBoard/Pieces/PieceFactory.cs
+++ ChessBoard/Pieces/PieceFactory.cs
@@ -17,6 +17,8 @@ namespace ChessBoard.Pieces
                     return new BlackPawn(board, coordinate);
                 case KNIGHT:
                     return new Knight(board, coordinate, color);
+                case ROOK:
+                    return new Rook(board, coordinate, color);
                 default:
                     throw new Exception("Meow meow problem meow");
             }
