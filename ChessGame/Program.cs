9b7b7eda02191c1d02f69f5e3f739d869d12c61c
ChadScarborough <chad.d.scarborough@gmail.com>
2022-08-23 11:17:18 -0700

    Setup Main for live testing

--- ChessGame/Program.cs
+++ ChessGame/Program.cs
@@ -1,4 +1,36 @@
 ﻿using ChessGame.Games;
 
 StandardGame game = new StandardGame();
-Console.WriteLine(game.Board);
+int moveCounter = 0;
+while (true)
+{
+    try
+    {
+        Console.Clear();
+        Console.WriteLine(game.Board);
+        string? input = Console.ReadLine();
+        if (input == null) continue;
+        if (moveCounter % 2 == 0)
+        {
+            WhiteMove(input);
+            moveCounter++;
+            continue;
+        }
+        BlackMove(input);
+        moveCounter++;
+    }
+    catch
+    {
+        continue;
+    }
+}
+
+void WhiteMove(string input)
+{
+    game.WhitePlayer.MakeMoveFromStringInput(input);
+}
+
+void BlackMove(string input)
+{
+    game.BlackPlayer.MakeMoveFromStringInput(input);
+}
