5613d3ad4d437ae0acd19a14174c5947edfa1b63
ChadScarborough <chad.d.scarborough@gmail.com>
2022-08-22 17:44:58 -0700

    Add King

---
+++ ChessBoard/Pieces/MoveBehaviours/KingMoveBehaviour.cs
@@ -0,0 +1,33 @@
+﻿using ChessBoard.Boards;
+
+namespace ChessBoard.Pieces.MoveBehaviours
+{
+    public class KingMoveBehaviour : IMoveBehaviour
+    {
+        IBoard _board;
+        IPiece _piece;
+
+        public KingMoveBehaviour(IBoard board, IPiece piece)
+        {
+            _board = board;
+            _piece = piece;
+        }
+
+        //TODO: Add logic for castling and preventing moving into or remaining in check
+
+        public bool CanMove(Coordinate targetLocation)
+        {
+            Coordinate currentLocation = _piece.Location.GetCoordinate();
+            if (currentLocation.Equals(targetLocation))
+                return false;
+            if (_board.GetSquareByCoordinate(targetLocation).IsOccupied() &&
+                _board.GetSquareByCoordinate(targetLocation).OccupyingPieceColor() == _piece.PieceColor)
+                return false;
+            int rankDiff = Math.Abs(currentLocation.rank - targetLocation.rank);
+            int fileDiff = Math.Abs(currentLocation.rank - targetLocation.rank);
+            if (rankDiff > 1 || fileDiff > 1)
+                return false;
+            return true;
+        }
+    }
+}
